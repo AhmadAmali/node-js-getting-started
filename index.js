6dd3712239c8520c7ad73670e5024e8505b460ae
AhmadAbroad22 <ahmadamali77@gmail.com>
2020-07-25 00:57:49 -0600

    fixed details

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var http = require('http');
+var fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
